98a62a9c1621a404f4544c46c9d1ea51333836b4
Kadhar <Kadhar@DESKTOP-1LLLVQ7>
2019-09-16 21:25:37 +0530

    Final test

--- src/test/java/testng/NewTest1.java
+++ src/test/java/testng/NewTest1.java
@@ -2,12 +2,10 @@ package testng;
 
 import org.testng.annotations.Test;
 
-import java.awt.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -20,23 +18,21 @@ public void launch(){
   System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
   driver = new ChromeDriver();
   driver.manage().window().maximize();
-  driver.get("http://3.9.177.35/");
+  driver.get("http://3.8.173.3/");
   driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
   String x= driver.getTitle();
   System.out.println(x);
   }
 @Test
 public void verifytest() {
- 
- //driver.findElement(By.id("twotabsearchtextbox")).sendKeys("mi a3 mobile");
+	
  driver.findElement(By.xpath("//a[@href='/content/about-us.php']")).click();
- String text = "testing";
-java.util.List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + text  + "')]"));
- Assert.assertTrue(list.size() > 0, "Text not found!");
- 
+ String bodyText = driver.findElement(By.xpath("//p[@id='PID-ab2-pg']")).getText();
+ Assert.assertTrue(bodyText.contains("text"), "Text not found!");
+ //Assert.assertTrue(bodyText.contains("Basha"));
 }
 @AfterMethod
 public void close(){
-  //driver.close();
+  driver.close();
 }
 }
